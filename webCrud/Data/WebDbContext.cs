08763670a62f9d7eef27f97c3a15e0cb4668ac2d
NAJEER M K <70833723+najeermk@users.noreply.github.com>
2023-11-05 08:30:02 +0530

    first commit

---
+++ webCrud/Data/WebDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using webCrud.Models.Domain;
+
+namespace webCrud.Data
+{
+    public class WebDbContext : DbContext
+    {
+        public WebDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ webCrud/Models/Domain/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace webCrud.Models.Domain
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+
+        [DisplayName("Name")]
+        [Required(ErrorMessage = "This field is required.")]
+        public string Name { get; set; }
+
+        [DisplayName("Email")]
+        [Required(ErrorMessage = "This field is required.")]
+        public string Email { get; set; }
+
+        [DisplayName("Salary")]
+        [Required(ErrorMessage = "This field is required.")]
+        public long Salary { get; set; }
+
+        [DisplayName("Date of Birth")]
+        [Required(ErrorMessage = "This field is required.")]
+        [DisplayFormat(DataFormatString = "{0:MMM-dd-yy}")]
+        public DateTime DateOfBirth { get; set;}
+        public string Department { get; set; }
+    }
+}
